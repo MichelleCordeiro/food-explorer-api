837b3be3e4c5a93a08b0d06fee7cd3da1e400256
Michelle Cordeiro <mic321@gmail.com>
2025-05-08 04:32:37 -0300

    feat:  improve dishes and ingredients migrations

--- src/database/knex/migrations/20240323012109_createDishes.js
+++ src/database/knex/migrations/20240323012109_createDishes.js
@@ -1,12 +1,16 @@
 exports.up = knex =>
   knex.schema.createTable('dishes', table => {
     table.increments('id')
+
     table.text('name').notNullable()
-    table.text('description').default('A descrição será adicionada em breve.')
-    table.text('image').notNullable()
-    table.text('category')
-    
-    table.float('price', 5, 2)
+    table.text('description').notNullable()
+    table.text('category').notNullable()
+    table.text('image').default(null)
+
+    table.decimal('price', 8, 2).notNullable()
+
+    table.integer('created_by').unsigned().references('id').inTable('users').onDelete('SET NULL')
+    table.integer('updated_by').unsigned().references('id').inTable('users').onDelete('SET NULL')
     table.timestamp('created_at').default(knex.fn.now())
     table.timestamp('updated_at').default(knex.fn.now())
   })
