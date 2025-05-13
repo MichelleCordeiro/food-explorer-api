baa81cf8c9e8207d94c146c67994c90938253dc9
Michelle Cordeiro <mic321@gmail.com>
2025-05-13 00:55:05 -0300

    feat: add SessionsController and routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 
 const usersRouter = require('./users.routes')
+const sessionsRouter = require('./sessions.routes')
 const dishesRouter = require('./dishes.routes')
 const ordersRouter = require('./orders.routes')
 const favoritesRouter = require('./favorites.routes')
@@ -8,6 +9,7 @@ const favoritesRouter = require('./favorites.routes')
 const routes = Router()
 
 routes.use('/users', usersRouter)
+routes.use('/sessions', sessionsRouter)
 routes.use('/dishes', dishesRouter)
 routes.use('/orders', ordersRouter)
 routes.use('/favorites', favoritesRouter)
